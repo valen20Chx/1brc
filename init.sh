3fc237878d1f25e1066a1603feab7b56cb795819
Valentin Charoux <valentin@charoux.com>
2024-02-26 03:29:21 +0100

    fix: Fix init script, wrong file names

--- init.sh
+++ init.sh
@@ -4,19 +4,22 @@ if [ ! -d "./1brc" ]; then
 	git clone https://github.com/gunnarmorling/1brc.git
 fi
 
-# Build the project
-if [ ! -f "./1brc/mesurements.txt" ]; then
-	eval "(cd ./1brc; ./mvnw clean verify)"
-
-	# Create the measurements
-	eval "(cd ./1brc; ./create_measurements.sh 1000000000)"
-	mv ./1brc/mesurements.txt ./mesurements.txt
+# Create the measurements
+if [ ! -f "./measurements_e9.txt" ]; then
+	# Build the project
+	if [ ! -f "./1brc/measurements.txt" ]; then
+		eval "(cd ./1brc; ./mvnw clean verify)"
+		eval "(cd ./1brc; ./create_measurements.sh 1000000000)"
+	fi
+	mv ./1brc/measurements.txt ./measurements_e9.txt
 fi
 
-if [ ! -f "./1brc/mesurements_e3.txt" ]; then
-	head -n 1000 ./mesurements.txt >mesurements_e3.txt
+if [ ! -f "./1brc/measurements_e3.txt" ]; then
+	head -n 1000 ./measurements.txt >measurements_e3.txt
 fi
 
-if [ ! -f "./1brc/mesurements_e6.txt" ]; then
-	head -n 1000000 ./mesurements.txt >mesurements_e6.txt
+if [ ! -f "./1brc/measurements_e6.txt" ]; then
+	head -n 1000000 ./measurements.txt >measurements_e6.txt
 fi
+
+echo "Done!"

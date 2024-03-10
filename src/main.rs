d30bc215c767b3669d563dfd5517f5d9a7118c7d
Valentin Charoux <valentin@charoux.com>
2024-03-10 04:19:45 +0100

    refactor: go back to BufReader::lines
    BufReader already reads a large block from the file.

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ enum ReadLineError {
     TempParseFailed,
 }
 
-fn read_line(buffer: String) -> Result<(String, i32), ReadLineError> {
+fn read_line(buffer: &str) -> Result<(String, i32), ReadLineError> {
     let (city, temp_str) = buffer
         .trim()
         .split_once(";")
@@ -55,59 +55,28 @@ fn main() -> io::Result<()> {
     let mut measurement_counts = HashMap::<String, MeasurementCounter>::new();
 
     let file = File::open(file_path)?;
-    let mut buf_reader = BufReader::new(file);
+    let buf_reader = BufReader::new(file);
 
-    let mut buffer = [0u8; 512];
-    let mut tail = String::new();
-    let mut partial_chars = Vec::<u8>::new();
-
-    while buf_reader.read(&mut buffer)? > 0 {
-        let mut new_buf = partial_chars.clone();
-        new_buf.extend_from_slice(&buffer);
-        let new_buf_ref = new_buf.as_ref();
-        let mut content: String = match std::str::from_utf8(new_buf_ref) {
-            Ok(content) => {
-                partial_chars.clear();
-                content.to_string()
-            }
-            Err(err) => {
-                let content =
-                    std::str::from_utf8(new_buf_ref[..err.valid_up_to()].as_ref()).unwrap();
-                partial_chars = new_buf[err.valid_up_to()..].to_vec();
-                content.to_string()
-            }
-        };
-
-        content.insert_str(0, tail.as_str());
-
-        if !content.ends_with("\n") {
-            let last_line = content.rsplit("\n").next().unwrap();
-            tail = last_line.to_string();
-            content = content[..content.len() - last_line.len()].to_string();
+    for line_res in buf_reader.lines() {
+        let line = line_res?;
+        if let Ok((city, temp)) = read_line(&line) {
+            update_map(&mut measurement_counts, city, temp);
         } else {
-            tail.clear();
-        }
-
-        for line in content.split("\n") {
-            if !line.is_empty() {
-                let (city, temp) = read_line(line.into()).unwrap_or_else(|_| {
-                    eprintln!("Failed to parse line: {}", line);
-                    panic!();
-                });
-                update_map(&mut measurement_counts, city, temp);
-            }
+            eprintln!("Failed to parse line: {}", line);
         }
     }
 
-    measurement_counts.iter().for_each(|(key, measurement)| {
-        let min = measurement.min;
-        let max = measurement.max;
-        let sum = measurement.sum;
-        let count = measurement.count;
-        let average = sum / i64::from(count);
+    for (city, data) in measurement_counts {
+        let average = data.sum / i64::from(data.count);
 
-        println!("{key};{min};{max};{average}")
-    });
+        println!(
+            "{key};{min};{max};{average}",
+            key = city,
+            min = data.min,
+            max = data.max,
+            average = average
+        );
+    }
 
     Ok(())
 }

8f36289a1b4506939011538d6d2c24129a4bc201
Valentin Charoux <valentin20charoux@gmail.com>
2024-07-28 20:16:37 +0200

    build: rework init script

--- init.sh
+++ init.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-if [ ! -d "./1brc" ]; then
-	git clone https://github.com/gunnarmorling/1brc.git
+if [ ! -d "./1brc-base" ]; then
+	git clone https://github.com/gunnarmorling/1brc.git 1brc-base
 fi
 
 # Create the measurements
 if [ ! -f "./measurements_e9.txt" ]; then
 	# Build the project
-	if [ ! -f "./1brc/measurements.txt" ]; then
-		eval "(cd ./1brc; ./mvnw clean verify)"
-		eval "(cd ./1brc; ./create_measurements.sh 1000000000)"
+	if [ ! -f "./1brc-base/data/measurements.txt" ]; then
+		(
+			cd ./1brc-base/src/main/python/;
+			python3 ./create_measurements.py 1000000000;
+		);
 	fi
-	mv ./1brc/measurements.txt ./measurements_e9.txt
+	mv ./1brc-base/data/measurements.txt ./measurements_e9.txt;
 fi
 
-if [ ! -f "./1brc/measurements_e3.txt" ]; then
-	head -n 1000 ./measurements.txt >measurements_e3.txt
+if [ ! -f "./measurements_e3.txt" ]; then
+	head -n 1000 ./measurements_e9.txt > measurements_e3.txt
 fi
 
-if [ ! -f "./1brc/measurements_e6.txt" ]; then
-	head -n 1000000 ./measurements.txt >measurements_e6.txt
+if [ ! -f "./measurements_e6.txt" ]; then
+	head -n 1000000 ./measurements_e9.txt > measurements_e6.txt
 fi
 
 echo "Done!"
